693fc8f3d1ab175d73a7cd4a4990f9795a7a901c
Raul Contreras <raul@connect.financial>
2022-06-13 07:38:26 -0500

    chore: remove boilerplate code

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,6 @@ import {
 } from "@remix-run/react";
 
 import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
 
 export const links: LinksFunction = () => {
   return [
@@ -33,16 +32,6 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
   "X-UA-Compatible": "ie=edge",
 });
-
-type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  return json<LoaderData>({
-    user: await getUser(request),
-  });
-};
 
 export default function App() {
   return (
